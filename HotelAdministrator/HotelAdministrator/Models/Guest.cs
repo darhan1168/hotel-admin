f7eb923cc30ce47c6ed20959f6ace95ae44559df
darhan1168 <darinahanina04@gmail.com>
2023-05-06 16:50:47 +0200

    Create model for guest
    And different fields

---
+++ HotelAdministrator/HotelAdministrator/Models/Guest.cs
@@ -0,0 +1,10 @@
+namespace HotelAdministrator.Models;
+
+public class Guest : BaseEntity
+{
+    public string Name { get; set; }
+    public string SurName { get; set; }
+    public int NumPassport { get; set; }
+    public DateTime CheckOut { get; set; }
+    public HotelRoom HotelRoom { get; set; }
+}
